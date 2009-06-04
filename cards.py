5fba304e7985f994aa761876af0f4ed4ae8c618b
Nathaniel Rook <nrook@nrook-laptop-linux.(none)>
2009-06-04 14:59:03 -0400

    Added cards.py.
    - cards.py was accidentally left untracked.  This update fixes that.

---
+++ cards.py
@@ -0,0 +1,98 @@
+import rng
+
+MON_ABILITY_DICT = {
+    "lancer" : {"code":"CRITICAL", "aname":"Damaging attack"}
+}
+
+class Deck(object):
+    """
+    A Deck is a list of Cards held by the Player.
+
+    It consists of two main groups: the Hand, which includes the cards the
+    player can use right now, and the Library, which stores the rest.  The Hand
+    can store a maximum of seven cards; every turn, if there is room, a card is
+    moved from the Library to the Hand.  When the Player acquires a new Card by
+    killing an enemy, this Card is inserted at a random position in the Library.
+
+    Fields:
+    hand - a list of the Cards in the Hand.  The first card in the hand is
+        hand[0]; the last is hand[-1].
+    library - a list of the Cards in the Library.  The lowest card in the
+        library is hand[0]; the top card (i.e. the first to be drawn) is
+        hand[-1].
+    HAND_MAX_LENGTH - the maximum length of a hand.  Don't touch.
+    """
+
+    HAND_MAX_LENGTH = 7
+
+    def __init__(self, hand_list = None, library_list = None):
+        """
+        Create a new Deck, with a hand composed of the cards in hand_list and a
+        library composed of the cards in library_list.  If hand_list contains
+        more than seven cards, a ValueError will be thrown.
+        """
+
+        if hand_list is None:
+            hand_list = []
+        if library_list is None:
+            library_list = []
+        
+        if len(hand_list) > self.HAND_MAX_LENGTH:
+            raise ValueError("The hand provided contains %d > %d objects."
+                             % (len(hand_list), self.HAND_MAX_LENGTH))
+        self.hand = [card for card in hand_list]
+        self.library = [card for card in library_list]
+
+    def randomInsert(self, card):
+        """
+        Insert the card given into a random part of the Library.
+        """
+
+        rng.random_insert(self.library, card)
+
+    def draw(self):
+        """
+        If the Hand is not full, pop the top card off the Library and append it
+        to the end of the Hand.  If the Hand is full, do nothing.  Return True
+        if a card is drawn, and False if one is not.
+        """
+        if len(self.hand) <= self.HAND_MAX_LENGTH:
+            if len(self.library) >= 1:
+                self.hand.append(self.library.pop())
+                return True
+            else:
+                return False
+        elif len(self.hand) == self.HAND_MAX_LENGTH:
+            return False
+        else:
+            assert False, "The hand has more than %d cards!" \
+                % self.HAND_MAX_LENGTH
+
+class Card(object):
+    """
+    A thing held by the Player, used to activate some special ability.
+
+    Fields:
+    action_code - the special code of the special ability used.
+    monster_name - the name of the monster from whom the card comes.
+    ability_name - the name of the special ability the Card contains.
+    """
+
+    def __init__(self, action_code, monster_name, ability_name):
+        """
+        Create a new Card, with the action code, monster name, and ability name
+        specified.  For more information on these fields, see the general class
+        description.
+        """
+
+        self.action_code = action_code
+        self.monster_name = monster_name
+        self.ability_name = ability_name
+
+def mon_card(mon):
+    """
+    Return the card corresponding to the monster, "mon".
+    """
+
+    mon_attr_dict = MON_ABILITY_DICT[mon.name]
+    return Card(mon_attr_dict["code"], mon.name, mon_attr_dict["aname"])
