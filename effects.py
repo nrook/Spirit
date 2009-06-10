70914c55222e1ddab0bd03768ee897e236fdec1d
Nathaniel Rook <nrook@nrook-laptop-linux.(none)>
2009-06-10 17:56:28 -0400

    Added effects.py, which contains the effectsMap function (and a bunch of things which don't work).
    - Added effects.py to source control.
    - Added effectsMap(), which returns a dict which returns the transparent glyph
        if the thing searched for is missing.

---
+++ effects.py
@@ -0,0 +1,44 @@
+"""
+Creates and manages "special effects" to be used on the screen.
+"""
+
+import collections
+
+import tcod_display as display
+import threading
+import config
+
+BLINKING_TIME = 0.7
+
+thr = None
+ev = None
+
+def init():
+    """
+    Initialize the thread responsible for animating effects.
+    """
+
+    global thr
+    global ev
+
+    ev = threading.Event()
+    thr = threading.Thread(target=repeat, args=(ev, BLINKING_TIME, animate))
+    thr.start()
+
+def repeat(event):
+    while True:
+        event.wait(BLINKING_TIME)
+        if event.isSet():
+            break
+        # do animation things
+
+def stop():
+    ev.set()
+
+def effectsMap():
+    """
+    Returns a map identical to a dictionary, except that it returns the
+    transparent glyph on a lookup failure.
+    """
+
+    return collections.defaultDict(lambda: config.TRANSPARENT_GLYPH)
