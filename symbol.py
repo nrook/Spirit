0a5f16f172cbae18d9e6125711d5ef23e1afc8ac
Nathaniel Rook <nrook@nrook-laptop-linux.(none)>
2009-06-17 14:52:42 -0400

    Added value equality (and hashing) to the Glyph class.

--- symbol.py
+++ symbol.py
@@ -28,6 +28,15 @@ class Glyph(object):
         self.g = color[1]
         self.b = color[2]
 
+    def __eq__(self, other):
+        return (self.char == other.char) and (self.color == other.color)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash((self.char, self.color))
+
 BAD_GLYPH = ('!', 255, 0, 0)
 
 class glyphMap(dict):
